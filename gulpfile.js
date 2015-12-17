f6ef95d50b526d87013f5a41430f7d99be00e9c6
Stijn Janssen <me@stijnjanssen.be>
2015-12-17 21:41:15 +0100

    improved gulp to also do the local serve part

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,48 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    express = require('express'),
+    sass = require('gulp-sass'),
+    spawn = require('child_process').spawn;
 
-gulp.task('default', function() {
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = '_site/'
+
+
+// Run Jekyll Build Asynchronously
+gulp.task('jekyll', function () {
+    var jekyll = spawn('jekyll', ['build']);
+
+    jekyll.on('exit', function (code) {
+        console.log('-- Finished Jekyll Build --')
+    })
+});
+
+
+// Compile SASS
+gulp.task('sass', function() {
     gulp.src('./assets/sass/style.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./assets/sass/**/*.scss', ['default']);
+
+// Run static file server
+gulp.task('serve', function () {
+    var server = express();
+    server.use(express.static(EXPRESS_ROOT));
+    server.listen(EXPRESS_PORT);
 });
+
+
+// Watch for changes
+gulp.task('watch', function () {
+    // Manually compile and inject css to avoid jekyll overhead
+    gulp.watch('assets/_scss/*.scss', ['sass:dev']);
+
+    // Watch for changes to other files for jekyll compilation
+    // Note: This will probably need to be updated with the files you want to watch
+    // Second Note: MAKE SURE that the last to items in the watchlist are included or else infinite jekyll loop
+    gulp.watch(['*.html', '*/*.html', '*/*.md', '!_site/**', '!_site/*/**'], ['jekyll']);
+})
+
+
+gulp.task('default', ['sass', 'jekyll', 'serve', 'watch']);
